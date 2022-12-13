863dbfbc1d74eb4119b3b77ae3f335e8563ebd6b
math92n4 <math92n4@stud.kea.dk>
2022-12-13 12:49:25 +0100

    arrays to string, for loop

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
 
 
@@ -7,6 +9,8 @@ public class Main {
         Person ron = new Person("Ron","Weasley");
         Person hermione = new Person("Hermione","Granger");
 
+        Person[] persons = {harry,ron,hermione};
+
         harry.setPoint(100);
         ron.setPoint(231);
         hermione.setPoint(420);
@@ -14,6 +18,17 @@ public class Main {
         System.out.println(harry);
         System.out.println(ron);
         System.out.println(hermione);
+
+        System.out.println("---------------\nARRAY:");
+
+        System.out.println(Arrays.toString(persons));
+
+        System.out.println("---------------\nENHANCED FOR LOOP:");
+
+        for (int i=0; i<persons.length; i++) {
+            System.out.println(Arrays.toString((persons)));
+        }
+
 
     }
 
