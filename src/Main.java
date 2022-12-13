05c7eadd809838b6fdb3c3e5d69456f5a74fe157
math92n4 <math92n4@stud.kea.dk>
2022-12-13 12:56:13 +0100

    new person, playing around with index

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,25 @@ public class Main {
         Person harry = new Person("Harry","Potter");
         Person ron = new Person("Ron","Weasley");
         Person hermione = new Person("Hermione","Granger");
+        Person ginny = new Person("Ginny","Weasley");
 
-        Person[] persons = {harry,ron,hermione};
+        Person[] persons = {harry,ron,hermione,ginny};
 
         harry.setPoint(100);
         ron.setPoint(231);
         hermione.setPoint(420);
+        ginny.setPoint(1000);
 
+        System.out.println("REGULAR PRINT:");
         System.out.println(harry);
         System.out.println(ron);
         System.out.println(hermione);
+        System.out.println(ginny);
+
+        persons[0] = harry;
+        persons[1] = ginny;
+        persons[2] = ron;
+        persons[3] = hermione;
 
         System.out.println("---------------\nARRAY:");
 
