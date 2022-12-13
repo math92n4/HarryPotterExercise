93c1a0c1f813389c0d40da9427dd893c5f678412
math92n4 <math92n4@stud.kea.dk>
2022-12-13 12:26:05 +0100

    class setup

---
+++ src/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int point;
+
+    public Person(String firstName,String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.point = 0;
+    }
+
+}
